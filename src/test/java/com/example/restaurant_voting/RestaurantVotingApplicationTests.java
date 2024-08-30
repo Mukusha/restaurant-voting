b693fc5f8f7f7150793ccd1ff6b3b11457fda01b
Mukusha <91588607+Mukusha@users.noreply.github.com>
2024-08-30 14:12:17 +0300

    inital - Spring Initializr

---
+++ src/test/java/com/example/restaurant_voting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
